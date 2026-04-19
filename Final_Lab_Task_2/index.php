68f7503e82f1c733d82797fa643c783c79c41a24
auti11 <sadiyamohonauti@gmail.com>
2026-04-19 14:35:46 +0600

    PUBLIC HOME

---
+++ Final_Lab_Task_2/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+
+<table border="1" width="100%">
+    <tr>
+        <td>
+            <h2><img src="img1.jpg" alt="" srcset="">XCompany</h2>
+        </td>
+        <td align="right">
+            <a href="index.php">Home</a> |
+            <a href="login.php">Login</a> |
+            <a href="registration.php">Registration</a>
+        </td>
+    </tr>
+
+    <tr>
+        <td colspan="2" height="200">
+            <h3>Welcome to XCompany</h3>
+        </td>
+    </tr>
+
+    <tr>
+        <td colspan="2" align="center">
+            Copyright © 2017
+        </td>
+    </tr>
+</table>
+
+</body>
+</html>
