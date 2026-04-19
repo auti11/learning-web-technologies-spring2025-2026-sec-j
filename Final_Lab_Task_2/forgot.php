74a473a71e1adced30500bae2382e77402c3a815
auti11 <sadiyamohonauti@gmail.com>
2026-04-19 18:30:12 +0600

    forgot

---
+++ Final_Lab_Task_2/forgot.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+    <title>Forgot Password</title>
+</head>
+<body>
+    <table border="1" width="80%" align="center">
+        <tr>
+            <td>
+                <h2>XCompany</h2>
+            </td>
+            <td align="right">
+                <a href="index.php">Home</a> |
+                <a href="login.php">Login</a> |
+                <a href="registration.php">Registration</a>
+            
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" align="center" height="200">
+                
+                
+                <form action="forgotCheck.php" method="post">
+                    <fieldset style="width:400px;">
+<legend>FORGOT PASSWORD</legend>
+                    Enter Email:<input type="email" name="email" >
+                    <hr>
+                    <input type="submit" name ="submit" value="Submit">
+                    </fieldset>
+                </form>
+            </td>
+        </tr>
+        <tr>
+<td colspan="2" align="center">Copyright © 2017</td>
+</tr>
+    </table>
+</body>
+</html>
